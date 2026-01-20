a73ecd6bd6a86a49e0fd48c5118351cd801bf311
FanetteE <fanette.emery@ensta-paris.fr>
2026-01-20 11:49:26 +0100

    definition des classes sudoku et grille

--- include/grille.hpp
+++ include/grille.hpp
@@ -1,1 +1,17 @@
-#include grille.hpp
+#ifndef grille.hpp
+#define grille.hpp
+#include <iostream>
+#include <vector>
+#include <complex>
+using namespace std ;
+using suint = unsigned int ;
+
+
+class Grille : vector<vector<int>>
+{public :
+    int n=0;                                         // ordre de la grille
+    mutable vector<pair<suint , suint>> casesVides ; // vecteur des cases vides
+    mutable vector<suint> vs , vl ,vc ,vb;           // vecteurs de travail
+};
+
+
---
+++ include/sudoku.hpp
@@ -0,0 +1,17 @@
+#ifndef sudoku.hpp
+#define sudoku.hpp
+#include <iostream>
+#include <vector>
+#include <complex>
+using namespace std ;
+using suint = unsigned int ;
+
+class Sudoku
+{ public :
+    int ordre=0; // ordre du Sudoku
+    int nbcase=0; // nombre de case à initialiser
+    bool allSol=false; // recherche de toutes les solutions si true
+    bool uniqueSol=false; // pour le test d’unicité
+    Grille grille_ini ; // grille initiale
+    list<Grille> grille_sol ; // liste des grilles solutions
+};
