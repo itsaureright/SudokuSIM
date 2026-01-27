caae93a075c6a6fc709f3adb8cc591395e5d1022
FanetteE <fanette.emery@ensta-paris.fr>
2026-01-27 11:24:19 +0100

    Constructeur + focntion isvalid + début de fonction Solve

--- include/sudoku.hpp
+++ include/sudoku.hpp
@@ -15,4 +15,54 @@ class Sudoku
     bool uniqueSol=false; // pour le test d’unicité
     Grille grille_ini ; // grille initiale
     list<Grille> grille_sol ; // liste des grilles solutions
+
+    Sudoku(int ordre, int nbcase, bool allSol, bool uniqueSol) ; 
+    void Solve() ;  // Pour résoudre la grille
+    bool isValid(const Grille& g, int ligne, int col, int val);  // Vérifie que le placement d'un chiffre est ok
+
+private:   
+    std::pair<int,int> blocCoords(int ligne, int col) const; // Coordonnées des blocs
 };
+
+//Constructeurs
+
+Sudoku::Sudoku(int ordre, int nbcase, bool allSol, bool uniqueSol) : ordre(ordre), nbcase(nbcase), allSol(allSol), uniqueSol(uniqueSol) {}
+
+//Fonctions
+
+std::pair<int,int> Sudoku::blocCoords(int ligne, int col) const { 
+    int n = ordre;
+    int blocL = (ligne / n) * n; 
+    int blocC = (col / n) * n; 
+    return {blocL, blocC}; }
+
+bool Sudoku::isValid(const Grille& g, int ligne, int col, int val)
+{   int n = ordre;
+    // Vérifier la ligne
+    for (int j = 0; j < g[ligne].size(); j++) {
+        if (j != col && g[ligne][j] == val)
+            return false;
+    }
+
+    // Vérifier la colonne
+    for (int i = 0; i < g.size(); i++) {
+        if (i != ligne && g[i][col] == val)
+            return false;
+    }
+
+    // vérifie le bloc
+    auto [blocL, blocC] = blocCoords(ligne, col); 
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if ((blocL + i != ligne || blocC + j != col) && g[blocL + i][blocC + j] == val){
+                return false; }
+            }
+        }
+
+    return true; 
+}
+
+void Sudoku::Solve() { 
+    vector<pair<suint, suint>> cases = grille_ini.casesVides;
+    
+}
