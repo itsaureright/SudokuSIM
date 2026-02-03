f6e7600ac643239a239e4ed2e52015ffa5ddf28b
itsaureright <lebiannicaure@gmail.com>
2026-02-03 10:06:51 +0100

    ajout de l'affichage

--- src/grille.cpp
+++ src/grille.cpp
@@ -119,3 +119,4 @@ void Grille::afficher() const
         std::cout << "-";
     std::cout << '\n';
 }
+
--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -1,1 +1,9 @@
 #include "sudoku.hpp"
+
+//Sudoku.cpp
+void Sudoku::afficherSolutions() const {
+    for (const auto& g : grilles_sol) {
+        g.afficher();
+        std::cout << '\n';
+    }
+}
