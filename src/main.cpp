66383b96317b6a310cbdb89b243e834dd9d8a1c7
FanetteE <fanette.emery@ensta-paris.fr>
2026-02-10 11:43:32 +0100

    mise en forme du main pour pouvoir proposer différents modes de jeux au joueur

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,43 @@ using suint = unsigned int ;
 int main(int argc, char *argv[])
 {
     srand(time(nullptr));
-
-    Sudoku S(3, 14, false, false); 
-    S.grille_ini.genererGrillePartielle(14);
-    S.grille_ini.afficher();
-    S.preparerGrilleUnique();
-    S.grille_ini.afficher();
-    S.Solve();
-    for (const Grille& sol : S.grille_sol) {
-    sol.afficher();
+    // Si l'utilisateur n'a rien passé en argument
+    if (argc < 2) {
+        cout << "Tape :\n"
+             << "  - 1 si tu veux generer une grille unique et sa solution\n"
+             << "  - 2 si tu veux jouer toi-meme au sudoku\n\n"
+             << "Puis relance le programme avec l'option choisie.\n"
+             << "Exemple : ./main 1\n";
+        return 0;
     }
 
+    // On récupère l'argument
+    string choix = argv[1];
 
+    if (choix == "1") {
+        cout << "Tu as choisi : generer une grille unique.\n"; 
+        int ordre; 
+        int nbCases;
+        cout << "Donne l'ordre de la grille (ex : 3 pour un 9x9) : "; cin >> ordre;
+        cout << "Donne le nombre de cases a remplir initialement (ce nombre peut augmenter pour avoir une grille unique) : "; cin >> nbCases;
+        cout << "\nGeneration d'une grille de taille " << ordre*ordre << "x" << ordre*ordre << " avec " << nbCases << " cases vides.\n";
+        Sudoku S(ordre, nbCases, false, false); 
+        cout << "Grille initiale :\n";
+        S.grille_ini.genererGrillePartielle(nbCases);
+        S.grille_ini.afficher();
+        cout << "Grille unique :\n";
+        S.preparerGrilleUnique();
+        S.grille_ini.afficher();
+        cout << "Solution unique :\n";
+        S.Solve();
+        for (const Grille& sol : S.grille_sol) {
+        sol.afficher();
+        }
+    }
+
+    else if (choix == "2") {
+        cout << "Mode 2 : jouer toi-meme au sudoku.\n";
+        // Ici tu appelles ton code de jeu
+    }
     return 0;
 }
